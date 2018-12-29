9ab8914ffe68295321d3f9d96ddb43fffa2da70e
1167050100 <diza41@gmail.com>
2018-12-29 21:43:44 +0700

    project paw

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/event', function () {
+    return view('event');
+});
+
+Route::get('/profile_masjid_mrb', function () {
+    return view('profile_masjid_mrb');
+});
+
+Route::get('/profile_masjid_pusdai', function () {
+    return view('profile_masjid_pusdai');
+});
+
+Route::get('/profile_masjid_tsb', function () {
+    return view('profile_masjid_tsb');
+});
